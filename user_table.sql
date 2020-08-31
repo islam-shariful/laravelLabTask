4ec38094de28e53284d6161303538a1a0e8179e9
shariful-islam-sharif <imdshariful171@gmail.com>
2020-09-01 02:22:06 +0600

    user_table.sql(db_file)

---
+++ user_table.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 31, 2020 at 10:20 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laravel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `type` varchar(32) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `department` varchar(32) NOT NULL,
+  `cgpa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_table`
+--
+
+INSERT INTO `user_table` (`userid`, `username`, `password`, `type`, `name`, `department`, `cgpa`) VALUES
+(1, 'sharif', '123', 'admin', 'Md. Shariful Islam', 'CSE', 3),
+(2, 'hossain', '123', 'user', 'Hossain Sharif', 'EEE', 4),
+(5, 'Islam', '123', 'admin', 'Islam, Md. Shariful', 'CSE', 4),
+(7, 'shihab', '123', 'user', 'Shihab Shahib', 'CSSE', 3),
+(8, 'raiyan', '123', 'admin', 'Islam, Raiyan', 'CSE', 4);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
